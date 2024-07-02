56d2c0d4300486e38b5c47e6f21bf0010db7aabf
Frost leaf <zx7unknow@gmail.com>
2024-07-02 13:23:44 +0000

    fs: ext4: fix error when compile
    - unterminated conditional directive
    - reapply stock code

--- fs/ext4/ext4.h
+++ fs/ext4/ext4.h
@@ -1552,14 +1552,20 @@ struct ext4_sb_info {
 	struct ratelimit_state s_warning_ratelimit_state;
 	struct ratelimit_state s_msg_ratelimit_state;
 
-	/*
-	 * Barrier between writepages ops and changing any inode's JOURNAL_DATA
-	 * or EXTENTS flag.
-	 */
+	/* Barrier between changing inodes' journal flags and writepages ops. */
+	struct percpu_rw_semaphore s_journal_flag_rwsem;
+	/* for discard command control */
+#if defined(VENDOR_EDIT) && defined(CONFIG_EXT4_ASYNC_DISCARD_SUPPORT)
+//yh@PSW.BSP.Storage.EXT4, 2018-11-26 add for ext4 async discard suppot
+	struct discard_cmd_control *dcc_info;
+	unsigned long last_time;	/* to store time in jiffies */
+	long interval_time;		/* to store thresholds */
 	struct percpu_rw_semaphore s_writepages_rwsem;
+	#endif
 
 	/* Encryption support */
 #ifdef CONFIG_EXT4_FS_ENCRYPTION
+#define EXT4_KEY_DESC_PREFIX_SIZE 5
 	u8 key_prefix[EXT4_KEY_DESC_PREFIX_SIZE];
 	u8 key_prefix_size;
 #endif
